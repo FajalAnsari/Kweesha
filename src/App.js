bc776499e1ddb42f04c296c4715834b8281ca09c
bobyswaroop <bobyswaroop555024@gmail.com>
2023-06-24 17:27:00 +0530

    read

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 
-
+import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home_page/Home';
+import Read_more from './components/Read_more_page/Read_more';
 function App() {
   return (
-    <div className="App">
-<Home />
+    <Router>
+         <div className="App">
+      <Routes>
+
+ 
+      <Route path="/" element={<Home />}/>
+      <Route path="read" element={<Read_more />}/>
+
+ 
+    </Routes>
     </div>
+    </Router>
   );
 }
 
--- src/Load_data/data.js
+++ src/Load_data/data.js
@@ -28,3 +28,28 @@ export const homepage = [
     
     }
 ]
+
+
+export const read_more_data = [
+    {
+        id:1,
+        title:'ABOUT US',
+        subtitle:'Empowering Business Growth through Tailored Solutions',
+        description:"Founded in 2023, Kweesha Solutions is Sitanshu Srivastava's brainchild. The concept? A think tank for businesses of all sizes and sectors. Over the years, we've expanded our operations, launching Kweesha Capital, Kweesha Cosmetics, and Kweesha Consulting, while there are more to come. At the heart of our diverse operations is a shared ethos: problem-solving, diversity, and sustainability. We approach each business challenge as an opportunity to demonstrate our unique ability to analyze, solve, and innovate.   We harness our expertise in finance, technology, science, and psychology to devise solutions that promote operational efficiency, financial robustness, and sustainable growth."
+    },
+    {
+        id:2,
+        title:'SOLUTIONS',
+        subtitle:'Unlocking Business Potential',
+        description:"Kweesha Solutions thrives on complexity. Our expert team of high IQ professionals is ready to delve into your business's operations, dissecting each element with an analytical lens. We unearth inefficiencies that often go unnoticed in day-to-day operations and design tailored strategies to rectify them. Our solutions are as diverse as your needs. From redefining operational workflows to enhancing financial analysis, we have the expertise to customize solutions for various sectors. We don't just help your business survive; we empower it to grow.  By identifying and focusing on key growth factors, we help your business move beyond survival mode and towards a future of sustainable success." 
+
+    },
+    {
+        id:3,
+        title:'CAREER',
+        subtitle:'Career Opportunities at Kweesha Solutions',
+        description:"At Kweesha Solutions, we're always on the lookout for talented individuals who share our passion for problem-solving and sustainability. We offer a myriad of career opportunities, spanning finance, technology, science, and psychology. Our stimulating work environment fosters creativity, innovation, and professional growth. Our job listings cover a diverse range of roles designed to cater to varying skill sets. With us, you'll find opportunities to solve complex business problems, contribute to sustainability, and help businesses move from surviving to thriving.  So, if you're smart, driven, and ready to make a difference, apply today and join our team." 
+
+    }
+   
+]
