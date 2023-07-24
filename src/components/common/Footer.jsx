cd88b874fad27f8b9827a4ec2cca3a602724f544
usha singh <ushasing215105@gmail.com>
2023-07-24 14:07:57 +0530

    new updates

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
             <section className="footer_section">
               <div className="container text-center footerp text-md-start mt-5">
                 <div className="row mt-3">
-                  <div class="col-md-3 col-lg-3 col-xl-3 col-12 mx-auto mt-3">
+                  <div class="col-md-4 col-lg-3 col-xl-3 col-12 mx-auto mt-3">
                     <img src={footer_logo} alt="kweesha_logo" className='w-75' />
                     <p className="text-dark text-decoration-none mt-4 add"><b>Head Office Address </b><br />
                       16 Navneet Vihar, Sector 9, Indira
@@ -24,7 +24,7 @@ const Footer = () => {
                       226021
                     </p>
                   </div>
-                  <div className="col-md-2 col-lg-2 col-xl-2 col-6 mx-auto s-1 mb-4">
+                  <div className="col-md-4 col-lg-2 col-xl-2 col-6 mx-auto s-1 mb-4">
                     <hr className="mb-2 mt-0 d-inline-block mx-auto footer_com" />
                     <h6 className="text-uppercase fw-bold text-success">Quick Link</h6>
                     <hr
@@ -47,7 +47,7 @@ const Footer = () => {
                     
                   </div>
 
-                  <div className="col-md-2 col-lg-2 col-xl-2 col-6 mx-auto mb-4 s-1">
+                  <div className="col-md-4 col-lg-2 col-xl-2 col-6 mx-auto mb-4 s-1">
                     <hr
                       className="mb-2 mt-0 d-inline-block mx-auto footer_com"
                     />
@@ -70,7 +70,7 @@ const Footer = () => {
                   </div>
 
                   
-                  <div className="col-md-5 col-lg-5 col-xl-5 col-12  mb-4 s-1">
+                  <div className="col-md-12 col-lg-5 col-xl-5 col-12  mb-4 s-1">
                     <hr
                       className="mb-2 mt-0 d-inline-block mx-auto footer_com"
                     />
@@ -97,13 +97,13 @@ const Footer = () => {
             />
             <section className="p-3 pt-0 mt-2 s-2">
               <div className="row d-flex align-items-center">
-                <div className="col-md-7 col-lg-8 text-center text-md-start">
+                <div className="col-md-5 col-lg-7  text-center text-md-start">
                   <div className="p-3 ps-1">
                     Copyright ©<span className='text-success'>2023-2024</span> | <Link to="/" className='text-success text-decoration-none'>Kweesha Pvt. Ltd.</Link>
                   </div>
                 </div>
 
-                <div className="col-md-5 col-lg-4 ml-lg-0  text-center text-md-end">
+                <div className="col-md-7 col-lg-5 ml-lg-0  text-center text-md-end">
                   <p className='btn text-success btn-floating m-1 '>Follow:</p>
 
                   <Link
