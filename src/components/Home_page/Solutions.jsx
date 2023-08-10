469479a504a8acfb5eb21b466905faaaa4e3fbb0
Fajal Ansari <fajalansari1997@gmail.com>
2023-08-10 03:50:30 -0400

    contact us and solution

--- src/components/Home_page/Solutions.jsx
+++ src/components/Home_page/Solutions.jsx
@@ -23,7 +23,7 @@ const Solutions = () => {
                   <h5 className='fs-4 title w-75 text-start'>Empowering Business Growth through Tailored Solutions</h5>
                   <p className='fs-6 mt-4 desc text-justify w-75 text-start justi'>Our solutions are as diverse as your needs. From redefining operational workflows to enhancing financial analysis, we have the expertise to customize solutions for various sectors. We don't just help your business survive; we empower it to grow.
                   <br /><br />
-                   By identifying and focusing on key growth factors, we help your business move beyond survival mode and towards a future of sustainable success.write a heading of this text</p>
+                   By identifying and focusing on key growth factors, we help your business move beyond survival mode and towards a future of sustainable success.</p>
                   <div className='border-0'>
                  <Link to="/solutions" className='text-decoration-none'><p className='btnss text-center text-white fw-bolder '>Learn more</p></Link>
                   </div>
